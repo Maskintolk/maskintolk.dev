4fbb7651d571e93fb5add0ac93555c7f4ce88f28
Jakob Skov <jakob.skov@gmail.com>
2021-10-27 19:50:28 +0200

    Change to floating sidebar and rounded corners

--- src/components/dev-project.js
+++ src/components/dev-project.js
@@ -4,24 +4,7 @@ const styles = css`
   :host {
     --width: 290px;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    min-height: 385px;
     background-color: var(--card-background-color);
-  }
-  .title {
-    display: flex;
-    align-items: flex-end;
-    padding-top: 20px;
-    max-height: 60px;
-    font-size: var(--font-size-header);
-    font-weight: 500;
-    text-transform: uppercase;
-    border-bottom: 2px solid var(--secondary-color);
-  }
-  div:nth-of-type(3) {
-    text-align: center;
   }
   h3 {
     font-size: var(--font-size-text);
@@ -30,10 +13,6 @@ const styles = css`
   p {
     font-size: var(--font-size-text);
     font-weight: 300;
-  }
-  .tech {
-    margin-top: 30px;
-    min-height: 122px;
   }
   a {
     text-decoration: none;
@@ -54,7 +33,28 @@ const styles = css`
     padding: 10px;
   }
   .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     padding: 0 20px;
+  }
+  .title {
+    display: flex;
+    align-items: flex-end;
+    padding-top: 20px;
+    height: 52px;
+    font-size: var(--font-size-header);
+    font-weight: 500;
+    text-transform: uppercase;
+    border-bottom: 2px solid var(--secondary-color);
+  }
+  .description {
+    height: 80px;
+  }
+  .tech {
+    margin-top: 30px;
+    min-height: 142px;
+    text-align: center;
   }
 `;
 
@@ -64,7 +64,7 @@ const template = (el) => html`
   </div>
   <div class="main">
     <div class="title">${el.name}</div>
-    <div>
+    <div class="description">
       <p>${el.description}</p>
     </div>
     <div class="tech">
